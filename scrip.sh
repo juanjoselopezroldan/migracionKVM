a44038b07b95717f154a5da22e6522be9e636be3
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 12:52:13 +0100

    cambiar expresion

--- scrip.sh
+++ scrip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
-ip1=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 
-)
+ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
 
 #while [[  ]]; do
 	#statements
