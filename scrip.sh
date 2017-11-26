e405bab0105e0b6854a5b2a2a3e00b7db5f4c26d
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 19:10:01 +0100

    primera prueba

--- scrip.sh
+++ scrip.sh
@@ -51,7 +51,7 @@ while [[ $bucle != "salir" ]]; do
 			rsh -i /home/kiki/.ssh/cloud.key root@$ip iptables -t nat -A POSTROUTING -s 192.168.1.1/24 -o eth0 -j MASQUERADE
 			
 			echo "Añadimos regla IPTable en la maquina Anfitriona para que pueda saber donde mandar la peticion"
-			iptables -t nat -A PREROUTING -i virbr1 -p tcp --dport 80 -j DNAT --to $ip
+			iptables -t nat -A PREROUTING -i virbr1 -p tcp --dport 80 -j DNAT --to "$ip"
 			
 			echo "Salimos del Bucle while"
 			bucle="salir"
@@ -69,7 +69,7 @@ while [[ $bucle != "salir" ]]; do
 		rsh -i /home/kiki/.ssh/cloud.key root@$ip mount /dev/vda /var/www/html/
 		
 		echo "Añadimos regla IPTables en la maquina virtual para que acepte peticiones de fuera de su red virutal y devuelva la peticion"
-		rsh -i /home/kiki/.ssh/cloud.key root@$ip iptables -t nat -A POSTROUTING -s 192.168.1.1/24 -o eth0 -j MASQUERADE
+		rsh -i /home/kiki/.ssh/cloud.key root@$ip iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -o eth0 -j MASQUERADE
 		
 		echo "Añadimos regla IPTable en la maquina Anfitriona para que pueda saber donde mandar la peticion"
 		iptables -t nat -A PREROUTING -i virbr1 -p tcp --dport 80 -j DNAT --to $ip
