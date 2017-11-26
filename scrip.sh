25fe2ad3090643953ee67bd75299bf055c45d63d
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 13:42:45 +0100

    control de maquina activa

--- scrip.sh
+++ scrip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-control="0"
+bucle="seguir"
 
-while [[ $control == "100" ]]; do
+while [[ $bucle != "salir" ]]; do
 	#Obtiene la informacion de la ocupacion de procesamiento en la maquina anfitriona
 	control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
 	#Obtiene la informacion del estado de la maquina
@@ -14,7 +14,9 @@ while [[ $control == "100" ]]; do
 		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
 	
 	else
+		#Iniciamos la maquina, esperamos 5 segundos y asociamos el volumen a la maquina
 		virsh -c qemu:///system start debian8-1
+		sleep 5
 		virsh -c qemu:///session attach-disk debian8-1 /dev/disco/lv1 vda
 		#Obtiene la ip de la maquina
 		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
@@ -24,6 +26,7 @@ while [[ $control == "100" ]]; do
 		rsh -i /home/kiki/.ssh/cloud.key root@$ip iptables -t nat -A POSTROUTING -s 192.168.1.1/24 -o eth0 -j MASQUERADE
 		#Añadimos regla IPTable en la maquina Anfitriona para que pueda saber donde mandar la peticion
 		iptables -t nat -A PREROUTING -i virbr1 -p tcp --dport 80 -j DNAT --to $ip
+		#En este punto podremos comprobar en el navegador como podemos acceder a la pagina si tenemos en el volumen algun index.html (recordad que el Apache tiene que esta configurado previamente)
 	fi
 done
 
