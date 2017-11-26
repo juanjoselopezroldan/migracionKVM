f142fa1bc219fe41714f7aa3c71a38b8af48561c
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 13:03:12 +0100

    comandos estado maquina

--- scrip.sh
+++ scrip.sh
@@ -2,8 +2,8 @@
 
 control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
 ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
-maquina=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 3)
 estadomq=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 4)
+
 if 
 
 #while [[  ]]; do
