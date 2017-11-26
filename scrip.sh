59a3fa9acb8fa14b2468a2c36dfb806c581f7a63
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 14:08:09 +0100

    control de maquina activa

--- scrip.sh
+++ scrip.sh
@@ -3,16 +3,22 @@
 bucle="seguir"
 
 while [[ $bucle != "salir" ]]; do
-	#Obtiene la informacion de la ocupacion de procesamiento en la maquina anfitriona
-	control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
+
 	#Obtiene la informacion del estado de la maquina
 	estadomq=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 4)
 
 	if [[ $estadomq == "running" ]] 
 	then
+		#Obtiene la informacion de la ocupacion de procesamiento en la maquina anfitriona
+		control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
+		
 		#Obtiene la ip de la maquina si esta en ejecucion
 		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
-	
+		
+		if [[ $control == "100" ]]
+		then
+			
+
 	else
 		#Iniciamos la maquina, esperamos 5 segundos y asociamos el volumen a la maquina
 		virsh -c qemu:///system start debian8-1
