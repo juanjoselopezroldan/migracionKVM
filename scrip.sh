12f79f6c9b28581e31c725a1e0327427bd7047b4
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 13:18:06 +0100

    generando estructura

--- scrip.sh
+++ scrip.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
-ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
-estadomq=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 4)
-
 while [[ $control == "100" ]]; do
+	#Obtiene la informacion del estado de la maquina de cara al proceso
 	control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
+	estadomq=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 4)
 
 	if [[ $estadomq == "running" ]] 
 	then
-		
+		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
+	
 	else
 		virsh -c qemu:///system start debian8-1
+		virsh -c qemu:///session attach-disk debian8-1 /dev/disco/lv1 vda
+		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
+
+
 	fi
 done
 
