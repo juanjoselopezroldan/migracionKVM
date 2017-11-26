ec01be56751e95d3a1a970ecc5fcafbc0cd273d5
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 20:18:51 +0100

    primera prueba

--- scrip.sh
+++ scrip.sh
@@ -41,6 +41,7 @@ while [[ $bucle != "salir" ]]; do
 			echo "Asociamos el volumen a la otra maquina"
 			virsh -c qemu:///session attach-disk debian8-2 /dev/disco/lv1 vda
 
+			sleep 15
 			echo "Obtiene la ip de la maquina que esta en ejecucion"
 			ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
 		
@@ -64,7 +65,7 @@ while [[ $bucle != "salir" ]]; do
 		
 		echo "Obteniendo la ip de la maquina"
 		ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
-		sleep 15
+		sleep 25
 		echo $ip
 		sleep 15
 		echo "Monta el volumen"
