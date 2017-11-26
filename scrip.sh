bef52bb4e20f8462a286b153c7edf794545b4a2c
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 13:10:34 +0100

    generando estructura

--- scrip.sh
+++ scrip.sh
@@ -4,11 +4,17 @@ control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head
 ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
 estadomq=$(virsh list --all | egrep "debian8-1" | tr -s " " | cut -d " " -f 4)
 
-if 
+while [[ $control == "100" ]]; do
+	control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
 
-#while [[  ]]; do
-	#statements
-#done
+	if [[ $estadomq == "running" ]] 
+	then
+		
+	else
+		virsh -c qemu:///system start debian8-1
+	fi
+done
+
 
 echo $control
 echo $ip
