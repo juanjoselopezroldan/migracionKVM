b3c93e15baf71524a13271067ba12f8ea2fb4989
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 16:46:06 +0100

    gestion de disco

--- scrip.sh
+++ scrip.sh
@@ -37,13 +37,21 @@ while [[ $bucle != "salir" ]]; do
 
 			#Desmontamos el volumen del directorio temporal
 			umount /mnt/
-			
+
 			#Asociamos el volumen a la otra maquina
 			virsh -c qemu://session attach-disk debian8-1 /dev/disco/lv1 vda
 
-
-
-
+			#Obtiene la ip de la maquina que esta en ejecucion
+			ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
+		
+			#Monta el volumen
+			rsh -i /home/kiki/.ssh/cloud.key root@$ip mount /dev/vda /var/www/html/
+		
+			#Añadimos regla IPTables en la maquina virtual para que acepte peticiones de fuera de su red virutal y devuelva la peticion
+			rsh -i /home/kiki/.ssh/cloud.key root@$ip iptables -t nat -A POSTROUTING -s 192.168.1.1/24 -o eth0 -j MASQUERADE
+			#Añadimos regla IPTable en la maquina Anfitriona para que pueda saber donde mandar la peticion
+			iptables -t nat -A PREROUTING -i virbr1 -p tcp --dport 80 -j DNAT --to $ip
+		
 	else
 		#Iniciamos la maquina, esperamos 5 segundos y asociamos el volumen a la maquina
 		virsh -c qemu:///system start debian8-1
