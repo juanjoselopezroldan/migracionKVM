b1cfc7e378a465b5da1d32a0104d1968dfa50a4a
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-26 12:59:15 +0100

    camb. expresion

--- scrip.sh
+++ scrip.sh
@@ -3,9 +3,10 @@
 control=$(ps aux | egrep libvirt+ | tr -s " " | cut -d " " -f 4 | sort -r | head -1 )
 ip=$(virsh net-dhcp-leases nat | tr -s " " | cut -d " " -f 6 | cut -d "/" -f 1 | tail -2)
 
+
 #while [[  ]]; do
 	#statements
 #done
 
 echo $control
-echo $ip1
+echo $ip
